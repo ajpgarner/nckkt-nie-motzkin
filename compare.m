d005dde863f8f7c9bd4d34169d333ea163adf988
Andrew Garner <6692740+ajpgarner@users.noreply.github.com>
2024-02-15 17:57:33 +0100

    Update compare.m

--- compare.m
+++ compare.m
@@ -5,8 +5,8 @@
 delta = 0.01;
 min_radius = 1.0;
 max_radius = 10.0;
-espilon = 1e-3; % For essential-ncKKT
-verbosity = 2;
+espilon = 1e-4; % For essential-ncKKT
+verbosity = 1;
 
 %% Set-up
 solver = NCNie(delta, min_radius, max_radius, verbosity);
@@ -19,10 +19,12 @@ npa_44 = solver.solve_without_kkt(4, 4, false);
 npa_54 = solver.solve_without_kkt(5, 4, false);
 
 %% ncKKT Solves:
-nckkt_325 = solver.solve_without_kkt(3, 2, 5, espilon, false);
-nckkt_335 = solver.solve_without_kkt(3, 3, 5, espilon, false);
-nckkt_437 = solver.solve_without_kkt(4, 3, 7, espilon, false);
-nckkt_447 = solver.solve_without_kkt(4, 4, 7, espilon, false);
+nckkt_321 = solver.solve_kkt(3, 2, 1, espilon, false);
+nckkt_332 = solver.solve_kkt(3, 3, 2, espilon, false);
+nckkt_433 = solver.solve_kkt(4, 3, 3, espilon, false);
+nckkt_443 = solver.solve_kkt(4, 4, 3, espilon, false);
+nckkt_544 = solver.solve_kkt(5, 4, 4, espilon, false);
+nckkt_545 = solver.solve_kkt(5, 4, 5, espilon, false);
 
 
 %% Display summary of results
@@ -33,9 +35,11 @@ fprintf("No KKT,\tMM = %d,\tLM = %d:\t%.8g\n", 4, 4, npa_44);
 fprintf("No KKT,\tMM = %d,\tLM = %d:\t%.8g\n", 5, 4, npa_54);
 fprintf("\n");
 
-fprintf("KKT,\tMM = %d,\tLM = %d,\tKKT = %d:\t%.8g\n", 3, 2, 5, nckkt_325);
-fprintf("KKT,\tMM = %d,\tLM = %d,\tKKT = %d:\t%.8g\n", 3, 3, 5, nckkt_335);
-fprintf("KKT,\tMM = %d,\tLM = %d,\tKKT = %d:\t%.8g\n", 4, 3, 7, nckkt_437);
-fprintf("KKT,\tMM = %d,\tLM = %d,\tKKT = %d:\t%.8g\n", 4, 4, 7, nckkt_447);
+fprintf("KKT,\tMM = %d,\tLM = %d,\tKKT = %d:\t%.8g\n", 3, 2, 1, nckkt_321);
+fprintf("KKT,\tMM = %d,\tLM = %d,\tKKT = %d:\t%.8g\n", 3, 3, 2, nckkt_332);
+fprintf("KKT,\tMM = %d,\tLM = %d,\tKKT = %d:\t%.8g\n", 4, 3, 3, nckkt_433);
+fprintf("KKT,\tMM = %d,\tLM = %d,\tKKT = %d:\t%.8g\n", 4, 4, 3, nckkt_443);
+fprintf("KKT,\tMM = %d,\tLM = %d,\tKKT = %d:\t%.8g\n", 5, 4, 4, nckkt_544);
+fprintf("KKT,\tMM = %d,\tLM = %d,\tKKT = %d:\t%.8g\n", 5, 4, 5, nckkt_545);
 fprintf("\n");
 
