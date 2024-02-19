ff9990abff2eb3e639c41cbc61e82353629af397
Andrew Garner <6692740+ajpgarner@users.noreply.github.com>
2024-02-19 14:28:20 +0100

    Update example_534.m

--- example_534.m
+++ example_534.m
@@ -4,15 +4,15 @@
 %% Parameters
 delta = 0.01;
 min_radius = 1.0;
-max_radius = 10.0;
-espilon = 1e-4; % For essential-ncKKT
+max_radius = 7.5;
+epsilon = 1e-4; % For essential-ncKKT
 verbosity = 1;
 
 %% Set-up
 solver = NCNieMotzkin(delta, min_radius, max_radius, verbosity);
 
 %% Solve:
-nckkt_534 = solver.solve_kkt(5, 3, 4, espilon, false);
+nckkt_534 = solver.solve_kkt(5, 3, 4, epsilon, false);
 fprintf("KKT,\tMM = %d,\tLM = %d,\tKKT = %d:\t%.8g\n", 5, 3, 4, nckkt_534);
 fprintf("\n");
 
